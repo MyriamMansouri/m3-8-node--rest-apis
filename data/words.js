1dfcd8deb654bb64dfe5b4ce3ee4ca12d49a76cd
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-14 16:55:18 -0400

    Added more words to array

--- data/words.js
+++ data/words.js
@@ -4,6 +4,51 @@ const words = [
     word: 'bacon',
     letterCount: '5',
   },
+  {
+    id: '1001',
+    word: 'cat',
+    letterCount: '3',
+  },
+  {
+    id: '1002',
+    word: 'computer',
+    letterCount: '8',
+  },
+  {
+    id: '1003',
+    word: 'painting',
+    letterCount: '8',
+  },
+  {
+    id: '1004',
+    word: 'curtain',
+    letterCount: '7',
+  },
+  {
+    id: '1005',
+    word: 'phone',
+    letterCount: '5',
+  },
+  {
+    id: '1007',
+    word: 'internet',
+    letterCount: '8',
+  },
+  {
+    id: '1008',
+    word: 'sofa',
+    letterCount: '4',
+  },
+  {
+    id: '1009',
+    word: 'rag',
+    letterCount: '3',
+  },
+  {
+    id: '1010',
+    word: 'mouse',
+    letterCount: '5',
+  },
 ];
 
 module.exports = { words };
